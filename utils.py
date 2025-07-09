69260d9bd959563d0b9f4d90aa31cd810e6f1a35
tehZevo <tehzevo@users.noreply.github.com>
2025-07-09 12:40:37 -0400

    Fix wave config

--- utils.py
+++ utils.py
@@ -2,9 +2,10 @@ import wave
 
 def save_audio_file(pcm_data, filename):
   with wave.open(filename, "wb") as wavfile:
-    channles = 1
+    channles = 2
     sample_width = 2
-    wavfile.setnchannels(1)
-    wavfile.setsampwidth(2)
-    wavfile.setframerate(48000)
+    sample_rate = 48000
+    wavfile.setnchannels(channles)
+    wavfile.setsampwidth(sample_width)
+    wavfile.setframerate(sample_rate)
     wavfile.writeframes(pcm_data)
