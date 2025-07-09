1f31a97aa37aeb33fadfb21a2450b03146836372
tehZevo <tehzevo@users.noreply.github.com>
2025-07-09 07:12:52 -0400

    Use temp file instead of dir

--- main.py
+++ main.py
@@ -8,27 +8,20 @@ from protopost import ProtoPost
 PORT = os.getenv("PORT", 80)
 MODEL = os.getenv("MODEL", "tiny.en")
 
+whisper_model = whisper.load_model(MODEL)
+
 #data should just be a b64 encoded string
 def handle(data):
-    with tempfile.TemporaryDirectory() as temp_dir:
-        #switch to new wd
-        old_dir = os.getcwd()
-        os.chdir(temp_dir)
+    tmp = tempfile.NamedTemporaryFile(delete=False)
 
+    with open(tmp.name, "wb") as f:
         #save audio file
         data = base64.b64decode(data)
-        with open("audio", "wb") as f:
-            f.write(data)
-        
-        #run whisper
-        subprocess.run(["whisper", "--model", MODEL, "--output_format", "txt", "audio"])
-
-        #load resulting file
-        with open("audio.txt", "r") as f:
-            text = f.read()
-        
-        #return to old wd
-        os.chdir(old_dir)
+        f.write(data)
+    
+    #run whisper
+    text = whisper_model.transcribe(tmp.name)
+    os.remove(tmp.name)
 
     #respond with text
     return text
