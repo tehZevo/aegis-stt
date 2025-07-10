4c6e79aa9c3f732f9d0119cd19b99e40c0648931
tehZevo <tehzevo@users.noreply.github.com>
2025-07-10 08:09:02 -0400

    Assume mp3

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import os
 import base64
-import subprocess
 from uuid import uuid4
 
 import whisper
 from protopost import ProtoPost
-
-from utils import save_audio_file
 
 PORT = os.getenv("PORT", 80)
 MODEL = os.getenv("MODEL", "tiny.en")
@@ -16,8 +13,11 @@ whisper_model = whisper.load_model(MODEL)
 #data should just be a b64 encoded string
 def handle(data):
     data = base64.b64decode(data)
-    filename = str(uuid4()) + ".wav"
-    save_audio_file(data, filename)
+    #TODO: use bytesio instead of saving to file
+    #see https://community.openai.com/t/openai-whisper-send-bytes-python-instead-of-filename/84786/4
+    filename = str(uuid4()) + ".mp3"
+    with open(filename, "rb") as f:
+        f.write(data)
     
     #run whisper
     text = whisper_model.transcribe(filename)
