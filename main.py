7ac090302ed29d47657e5a2980e8416a9c7c1141
tehZevo <tehzevo@users.noreply.github.com>
2025-07-11 10:34:44 -0400

    Log timings; add file extension

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import base64
+import time
 from uuid import uuid4
 
 import whisper
@@ -7,21 +8,34 @@ from protopost import ProtoPost
 
 PORT = os.getenv("PORT", 80)
 MODEL = os.getenv("MODEL", "tiny.en")
+FILE_EXT = os.getenv("FILE_EXT", "mp3")
 
 whisper_model = whisper.load_model(MODEL)
 
-#data should just be a b64 encoded string
 def handle(data):
+    t = time.time()
     data = base64.b64decode(data)
-    #TODO: use bytesio instead of saving to file
-    #see https://community.openai.com/t/openai-whisper-send-bytes-python-instead-of-filename/84786/4
-    filename = str(uuid4()) + ".mp3"
+    decode_time = time.time() - t
+
+    t = time.time()
+    filename = f"{str(uuid4())}.{FILE_EXT}"
+
     with open(filename, "wb") as f:
         f.write(data)
+    save_time = time.time() - t
     
     #run whisper
+    t = time.time()
     text = whisper_model.transcribe(filename)
     os.remove(filename)
+    whisper_time = time.time() - t
+
+    print(
+        "timings:",
+        f"decode: {round(decode_time, 2)}s,",
+        f"save: {round(save_time, 2)}s,",
+        f"whisper: {round(whisper_time, 2)}s"
+    )
 
     #respond with text
     return text
